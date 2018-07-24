bb289d17a8e2d275e458e8d9adc993d690edabbe
Yanuba <umberto961@live.it>
2018-07-24 10:15:36 +0200

    Added registration of fake device (still a stub)

--- FibersLKM.c
+++ FibersLKM.c
@@ -5,15 +5,50 @@
 #include "FibersLKM.h"
 
 
-static int __init fibers_init(void) 
+static long fibers_ioctl(struct file *, unsigned int, unsigned long);
+
+static int dev_major;
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .unlocked_ioctl = fibers_ioctl,
+    .compat_ioctl = fibers_ioctl, //for 32 bit on 64, works?
+};
+
+static long fibers_ioctl(struct file * filp, unsigned int cmd, unsigned long arg)
 {
-    printk(KERN_NOTICE "Module mounted");
+    switch(cmd) {
+        case 0:
+            printk(KERN_NOTICE "%s: Default ioctl called\n", KBUILD_MODNAME);
+            break;
+        default:
+            printk(KERN_NOTICE "%s: Default ioctl called\n", KBUILD_MODNAME);
+    }
+
     return 0;
+}
+
+static int __init fibers_init(void) 
+{   
+    int ret;
+
+    dev_major = register_chrdev(0, KBUILD_MODNAME, &fops);
+    if (dev_major < 0) {
+        printk(KERN_ERR "%s: Failed registering char device\n", KBUILD_MODNAME);
+        ret = dev_major;
+        goto fail_regchrdev;
+    }
+    printk(KERN_NOTICE "%s: Module mounted\n", KBUILD_MODNAME);
+    return 0;
+
+    fail_regchrdev:
+        return ret;
 }
 
 static void __exit fibers_exit(void)
 {
-    printk(KERN_NOTICE "Module un-mounted");
+    unregister_chrdev(dev_major, KBUILD_MODNAME);
+    printk(KERN_NOTICE "%s: Module un-mounted\n", KBUILD_MODNAME);
 }
 
 MODULE_LICENSE("GPL");
